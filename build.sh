44ed2c455d30a9d3d8812307fdeaf8ad31fb4679
Abdudhi 100 <dhikrullahabdullah93@gmail.com>
2026-04-20 19:17:40 +0100

    sevent commit for Beneficent Schools website

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ pip install -r requirements.txt
 export DJANGO_SETTINGS_MODULE=config.settings.production
 
 python manage.py migrate
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
